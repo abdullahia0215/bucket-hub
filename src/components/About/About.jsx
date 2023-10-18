0e5fcf0992448f659cfef05303f4088a9a62ad79
Abdi <abdullahia0215@gmail.com>
2023-10-18 11:24:41 -0500

    bones for about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,16 @@
-function About () {
+function About() {
     return (
         <div>
-            <h1>About</h1>
+            <h2>What Is BucketHub?</h2>
+            <p>BucketHub is a group task tracker where you can create, share, and combine your wildest dreams and bucket list adventures with your pals or for yourself. Whether it's climbing a mountain, going on a skiing trip, or skydiving we're your planning partner!</p>
+
+            <h2>Why I created BucketHub?</h2>
+            <p>I always wanted to create a way to provide my friend group with a way of tracking milestones that we've always wanted to hit, it's like a progress tracker!</p>
+
+            <h2>How Do I Use It?</h2>
+            <p>Simply click the My List tracker to access your personal list to add or view your goals or click the Group List to do the same! Only you can add to your personal list, but everyone can add to the group list</p>
         </div>
     );
 }
+
 export default About;
